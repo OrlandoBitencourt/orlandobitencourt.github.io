7c6641a2cbf08422b5e22f00db83fdcae73cc549
ORLANDO <orlando.cechlar@gmail.com>
2025-11-13 17:32:28 -0300

    Update comments.tsx

--- app/components/comments.tsx
+++ app/components/comments.tsx
@@ -1,24 +1,36 @@
 'use client'
 
-import Giscus from '@giscus/react'
+import { useEffect } from 'react'
 
 export default function Comments() {
-  return (
-    <div className="mt-12">
-      <Giscus
-        id="comments"
-        repo="https://OrlandoBitencourt/orlandobitencourt.github.io"
-        repoId="R_kgDOQSgBjQ"
-        category="General"
-        categoryId="DIC_kwDOQSgBjc4Cxv"
-        mapping="pathname"
-        reactionsEnabled="1"
-        emitMetadata="0"
-        inputPosition="top"
-        theme="noborder_dark"
-        lang="pt"
-        loading="lazy"
-      />
-    </div>
-  )
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.src = 'https://giscus.app/client.js'
+    script.setAttribute('data-repo', 'OrlandoBitencourt/ob-blog-comments')
+    script.setAttribute('data-repo-id', 'R_kgDOQU5iGg')
+    script.setAttribute('data-category', 'General')
+    script.setAttribute('data-category-id', 'DIC_kwDOQU5iGs4CltEV')
+    script.setAttribute('data-mapping', 'pathname')
+    script.setAttribute('data-strict', '0')
+    script.setAttribute('data-reactions-enabled', '1')
+    script.setAttribute('data-emit-metadata', '1')
+    script.setAttribute('data-input-position', 'top')
+    script.setAttribute('data-theme', 'noborder_dark')
+    script.setAttribute('data-lang', 'pt')
+    script.setAttribute('crossorigin', 'anonymous')
+    script.async = true
+
+    const comments = document.getElementById('comments')
+    if (comments) {
+      comments.appendChild(script)
+    }
+
+    return () => {
+      if (comments) {
+        comments.innerHTML = ''
+      }
+    }
+  }, [])
+
+  return <div id="comments" className="mt-12" />
 }
